088956626ccedc6dcb13e84130bac9928760dcdc
voronoi <adi@Adityas-MacBook-Pro.local>
2015-12-16 02:20:37 -0500

    Organized

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 */
 
 /*
-* File:   rr.cpp
+* File:   RuleRunner.cpp
 * Author: Aditya Daryanani
 *
 * Created on December 14, 2015, 12:17 PM
@@ -30,34 +30,38 @@ public:
 			cout << "\nRule RuleA has passed\n";
 			ruleCheck++;
 		}
-                else
-                {
-                    cout << "\nRule RuleA has failed\n";
-                }
+		else
+		{
+			cout << "\nRule RuleA has failed\n";
+		}
 		if (RuleB() == 1)
 		{
-			cout << "Rule RuleB has passed\n";
+			cout << "\nRule RuleB has passed\n";
 			ruleCheck++;
 		}
-                else
-                {
-                    cout << "\nRule RuleB has failed\n";
-                }
-                
+		else
+		{
+			cout << "\nRule RuleB has failed\n";
+		}
+
 		if (RuleC() == 1)
 		{
-			cout << "Rule RuleC has passed\n";
+			cout << "\nRule RuleC has passed\n";
 			ruleCheck++;
 		}
-                else
-                {
-                    cout << "\nRule RuleC has failed\n";
-                }
-                
+		else
+		{
+			cout << "\nRule RuleC has failed\n";
+		}
+
 		if (ruleCheck == NumberOfRules)
 		{
 			return 1;
 		}
+                if (ruleCheck == 0)
+                {
+                    cout << "\nALL rules failed\n";
+                }
 		return 0;
 	}
 
@@ -66,7 +70,7 @@ private:
 	{
 		for (int i = 1; i < arrayIndex; i++)
 		{
-			if ((integerArray[i - 1] + integerArray[i]) > 1000)
+			if ((integerArray[i - 1] + integerArray[i]) >= 1000)
 			{
 				return 1;
 			}
@@ -104,14 +108,26 @@ private:
 
 int main(int argc, char* argv[])
 {
-	int input[100];
-	rule Myrule;
-	for (int i = 1; i < argc; i++){
+	if (argc > 1) // Check if input arguments exist
+	{
+
+		int input[100];
+		rule Myrule;
+		for (int i = 1; i < argc; i++)
 		{
 			input[i - 1] = atol(argv[i]);
 		}
+
+		if (Myrule.RunTests(&input[0], argc - 1) == 1)
+		{
+			cout << "\nALL rules passed\n";
+			return 0;
+		}
 	}
-	if (Myrule.RunTests(&input[0], argc-1) == 1)
-		cout << "ALL rules passed\n";
+	else
+	{
+		cout << "\nNo input arguments\n";
+		return 0;
+	}
 	return 0;
 }
